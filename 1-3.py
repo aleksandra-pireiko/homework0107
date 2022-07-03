13eee21775592f66acfa6f2be7cc11b12e42c9ca
Alex Pireiko <aleksandra.pireiko@gmail.ru>
2022-07-03 23:17:43 +0300

    решение пунктов 1-3 задачи

---
+++ 1-3.py
@@ -0,0 +1,22 @@
+my_list = [42, 43]
+my_dict = {
+    "foo": {
+        "a": 12,
+        "b": (1, 2, 3, 4, my_list)
+    },
+    "bar": {
+        "c": 12,
+        "d": {5, 6, 7, 8}
+    },
+    "moo": {
+        "e": 12,
+        "f": {"Lol": ["L", "o", "l"]}
+    }
+}
+#1 Значение ключа "foo"
+print(my_dict["foo"])
+#2 Значение ключа "b"
+print(my_dict["foo"]["b"])
+#3 В my_list добавить 44
+my_list.append(44)
+print(my_list)
